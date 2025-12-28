e38ae704d5a5558ddc531083fe71183dce0e6f60
Mikensonthomas142 <mikensonthomas2@gmail.com>
2025-12-28 12:54:02 -0300

    Commit

--- src/pages/price/price_btc.tsx
+++ src/pages/price/price_btc.tsx
@@ -4,8 +4,19 @@ const API_URL = import.meta.env.VITE_API_URL;
 export function BTCPrice() {
   const [price, setPrice] = useState(0);
 
+  // useEffect(() => {
+  //   const ws = new WebSocket(`ws:${API_URL}/ws/btc`);
+
+  //   ws.onmessage = (event) => {
+  //     const data = JSON.parse(event.data);
+  //     setPrice(data.price);
+  //   };
+
+  //   return () => ws.close();
+  // }, []);
   useEffect(() => {
-    const ws = new WebSocket(`ws:${API_URL}/ws/btc`);
+    const wsUrl = `${API_URL.replace(/^http/, 'ws')}/ws/btc`;
+    const ws = new WebSocket(wsUrl);
 
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
@@ -14,6 +25,7 @@ export function BTCPrice() {
 
     return () => ws.close();
   }, []);
+
 
   return (
     <h2>BTC/USDT: ${price.toFixed(2)}</h2>
